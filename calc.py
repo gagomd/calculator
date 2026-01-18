ee406ec79631fbc65eaa1564cefe42a27b7fdb94
gago <mishagagoshidze@gmail.com>
2026-01-18 19:58:41 +0400

    Add operation Div

--- calc.py
+++ calc.py
@@ -1,9 +1,10 @@
 
-print("----Calculator!---")
+print("Calculator!")
 
 x = int(input("X = ")) # input x
-y = int(input("Y = ")) # input yy
+y = int(input("Y = ")) # input y
 
-print(f"Sum = {x+y}")  # sum
-print(f"Sub = {x-y}")  # sub
-print(f"Mult = {x*y}") # mult 
+print(f"Sum: {x + y}") # Sum
+print(f"Sub: {x - y}") # Sub
+print(f"Mul: {x * y}") # Mult 
+print(f"Div: {x / y}") # Div
