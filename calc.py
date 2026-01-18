362acda9c4f7bcd6d19f7e3a713c9c371da3391e
gago <mishagagoshidze@gmail.com>
2026-01-18 20:03:32 +0400

    Create div operation with check

--- calc.py
+++ calc.py
@@ -1,9 +1,15 @@
 
-print("----Calculator!---")
+print("Calculator!!!")
 
 x = int(input("X = ")) # input x
 y = int(input("Y = ")) # input yy
 
-print(f"Sum = {x+y}")  # sum
-print(f"Sub = {x-y}")  # sub
-print(f"Mult = {x*y}") # mult 
+print(f"Sum: {x + y}") # Sum
+print(f"Sub: {x - y}") # Sub
+print(f"Mul: {x * y}") # Mult
+
+if y==0:
+ print("Cant do it")
+else:
+ print(f"Div: { x / y}")
+
